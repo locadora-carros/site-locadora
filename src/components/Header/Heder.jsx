41b40a3d2a0b68584318fc4fb6409941d38207f2
jau italo <jau.batista@dcx.ufpb.br>
2023-08-29 16:11:32 -0300

    cabeçalho meio feito

--- src/components/Header/Heder.jsx
+++ src/components/Header/Heder.jsx
@@ -33,8 +33,57 @@ const Header = () => {
                 </Row>
             </Container>
         </div>
+
+
+        {/*========== Header middle ==========*/}
+        <div className="header__middle">
+            <Container>
+                <Row>
+                    <Col lg='4' md='3' sm='4'>
+                        <div className="logo">
+                            <h1>
+                                <Link to='/home' className="d-flex align-items-center gap-2">
+                                    <i class="ri-car-line"></i>
+                                    <span>
+                                        Los Santos<br />Company
+                                    </span>
+                                </Link>
+                            </h1>
+                        </div>
+                    </Col>
+
+                    <Col lg='3' md='3' sm='4'>
+                        <div className="header__location d-flex align-items-center gap-2">
+                            <span>
+                                <i class="ri-earth-line"></i>
+                            </span>
+                            <div className="header__location-contet">
+                                <h4>Paraiba</h4>
+                                <h6>Rio tinto, Paraiba</h6>
+                            </div>
+                        </div>
+                    </Col>
+
+                    <Col lg='3' md='3' sm='4'>
+                        <div className="header__location d-flex align-items-center gap-2">
+                            <span><i class="ri-time-line"></i></span>
+                            <div className="header__location-contet">
+                                <h4>Sunday to Friday</h4>
+                                <h6>10am - 7pm</h6>
+                            </div>
+                        </div>
+                    </Col>
+
+                    <Col lg='2' md='3' sm='0' className="text-end">
+                        <button className="header__btn btn">
+                            <Link to='/contact'>
+                                <i class="ri-phone-line"></i> Request a call
+                            </Link>
+                        </button>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     </header>
-
-
 }
 export default Header;
