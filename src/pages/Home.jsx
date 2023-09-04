5d4c4570c1e31c09089a8409f5f8ea9092642c21
JauItalo <jau.batista@dcx.ufpb.br>
2023-09-04 14:32:47 -0300

    hot bar feita

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ import { Container, Row, Col } from "reactstrap";
 import FindCarForm from "../components/UI/FindCarForm";
 import AboutSection from "../components/UI/AboutSection";
 import ServicesList from "../components/UI/ServiceList";
+import carData from '../assets/data/carData'
+import CarItem from "../components/UI/CarItem";
 
 const Home = () => {
     return (
@@ -42,6 +44,24 @@ const Home = () => {
                         </Col>
 
                         <ServicesList />
+                    </Row>
+                </Container>
+            </section>
+
+
+
+            {/* ========== car offer section ============ */}
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="12" className="text-center mb-5">
+                            <h6 className="section__subtitle">Come with</h6>
+                            <h2 className="section__title">Hot Offers</h2>
+                        </Col>
+
+                        {carData.slice(0, 6).map((item) => (
+                            <CarItem item={item} key={item.id} />
+                        ))}
                     </Row>
                 </Container>
             </section>
